b041855a9579f708b442616017677d939543cf2b
nayakunin <nayakunin99@gmail.com>
2022-12-10 18:14:07 +0100

    Fix

--- apps/predator-prey/vite.config.js
+++ apps/predator-prey/vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/projects/predator-prey',
+  base: '/projects/predator-prey/',
   plugins: [svgr(), react()],
   build: {
     sourcemap: true,
