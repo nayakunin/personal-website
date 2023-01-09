a251417863e852e60c12edf67459f97758e29100
nayakunin <nayakunin99@gmail.com>
2023-01-09 15:54:56 +0100

    Fix eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -24,6 +24,7 @@ module.exports = {
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
+    "import/no-unresolved": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
