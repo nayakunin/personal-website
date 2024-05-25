cce8c95770694e6d8dc2ee7cc29f0399d67b4616
Nikita Yakunin <nayakunin99@gmail.com>
2024-05-25 18:41:59 +0200

    fix linter

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,38 +1,40 @@
-module.exports = {
-  plugins: ["prettier", "@typescript-eslint", "simple-import-sort", "import"],
+const config = {
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'import'],
   extends: [
-    "turbo",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "prettier"
+    'turbo',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
   rules: {
-    "no-console": "warn",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "import/first": "error",
-    "import/newline-after-import": "error",
-    "import/no-duplicates": "error",
-    "import/no-unresolved": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    'no-console': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        "argsIgnorePattern": "^_"
-      }
+        argsIgnorePattern: '^_',
+      },
     ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/consistent-type-definitions": 0,
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/consistent-type-definitions': 0,
   },
 };
+
+module.exports = config;
