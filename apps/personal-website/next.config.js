99e278c2fa3712e4bbd75bfe792d20a59f8038b1
Nikita Yakunin <nayakunin99@gmail.com>
2024-05-26 14:53:37 +0200

    add redirect

--- apps/personal-website/next.config.js
+++ apps/personal-website/next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['shared'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/resume',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
